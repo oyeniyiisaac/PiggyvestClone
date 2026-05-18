006eab6ed24deecb92d9031375efdf1bb7b42f15
oyeniyiisaac <isaacoyeniyi6@gmail.com>
2026-05-18 09:33:56 +0100

    fix: update image sources in Feature and Footer components to use external URLs and base64 encoded images

--- src/component/Feature.jsx
+++ src/component/Feature.jsx
@@ -7,12 +7,12 @@ const feature = () => {
             <div className='feature-container'>
                 <p>As featured in</p>
                 <div className='feature'>
-                    <img src="src/img/tclogo@2x.png" alt="" />
-                    <img src="src/img/output.png" alt="" />
-                    <img src="src/img/brand1.png" alt="" />
-                    <img src="src/img/pym-removebg-preview.svg" alt="" />
-                    <img src="src/img/fast.svg" alt="" />
-                    <img src="src/img/cio.svg" alt="" />
+                    <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/tclogo_2x_e3771e8b17/tclogo_2x_e3771e8b17.png" alt="" />
+                    <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/output_9290be116f/output_9290be116f.png" alt="" />
+                    <img src="shttps://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/brand1_b2a9805fff/brand1_b2a9805fff.png" alt="" />
+                    <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/pym_removebg_preview_109a666426/pym_removebg_preview_109a666426.svg" alt="" />
+                    <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/fast_61fdc50524/fast_61fdc50524.svg" alt="" />
+                    <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/cio_6806ee3afd/cio_6806ee3afd.svg" alt="" />
                 </div>
             </div>
         </>

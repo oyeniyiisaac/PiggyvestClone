5edd48c15bad6d5ef3a21a7f31822470c778fca1
oyeniyiisaac <isaacoyeniyi6@gmail.com>
2026-05-18 08:26:15 +0100

    fix: update logo image source in NavBar component

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
             <nav className="navbar navbar-expand-lg ">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
-                        <img src="./src/img/image.png" alt="Logo" width="100%" height="34" className="d-inline-block align-text-top" />
+                        <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/logo_08e6965627/logo_08e6965627.svg" alt="Logo" width="100%" height="34" className="d-inline-block align-text-top" />
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>

a94c9a34e8b9b803e7a6a7bad8e0085975883117
oyeniyiisaac <isaacoyeniyi6@gmail.com>
2026-05-18 09:26:22 +0100

    fix: update image sources in OpenHouseCard and Testimony components to use external URLs

--- src/component/OpenHouseCard.jsx
+++ src/component/OpenHouseCard.jsx
@@ -8,10 +8,10 @@ const OpenHouseCard = () => {
                 <div className='open_house_card1'>
                     <div className='open_house_card1_content'>
                         <div>
-                            <img src="src/img/open_house.avif" alt="" />
+                            <img src="https://www.piggyvest.com/_next/static/images/cardBox1-63bf46c5c25194bf3fae4881ad465165.avif" alt="" />
                         </div>
                         <div className='open_house_card1_content_text'>
-                            <img src="src/img/Open_House_head.svg" alt="" />
+                            <img src="https://www.piggyvest.com/_next/static/images/homeOpenHSEDesktop-e0a8da474ff853c838518ff4ee76b7ca.svg" alt="" />
                             <p>Piggyvest OpenHouse is an annual community town-hall where we connect with our users, share product updates, answer questions, play games, and receive real-time feedback on our products.</p>
                             <button className='open_house_btn'>Learn More <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 24 24">
                                 <path d="M0 0h24v24H0z" fill="none" />
--- src/component/Testimony.jsx
+++ src/component/Testimony.jsx
@@ -8,22 +8,22 @@ const Testimony = () => {
                 <h2 className='testimony-title' data-aos="fade-up">Join over 6 million people who <br /> save and invest with us</h2>
                 <div className='testimony-cards'>
                     <div className='testimony-card' data-aos="fade-down" data-aos-delay="100">
-                        <img src="src/img/card1.jpeg" alt="" />
+                        <img src="https://storage.googleapis.com/piggybankservice.appspot.com/_story_pics/img725396eb22.jpeg" alt="" />
                         <p>Hi lovelies, my name is Ezinne, I came to know about piggy vest from a very close friend of mine, wh...</p>
                         <h2>- Ezinne B</h2>
                     </div>
                     <div className='testimony-card' data-aos="fade-down" data-aos-delay="200">
-                        <img src="src/img/card2.jpeg" alt="" />
+                        <img src="https://storage.googleapis.com/piggybankservice.appspot.com/_story_pics/img6ef4f48b4b.jpeg" alt="" />
                         <p>They help me to have extra savings apart <br /> from my bank account and it's easy and <br /> trusted</p>
                         <h2>—Faousiyah A</h2>
                     </div>
                     <div className='testimony-card' data-aos="fade-down" data-aos-delay="300">
-                        <img src="src/img/card3.jpeg" alt="" />
+                        <img src="https://storage.googleapis.com/piggybankservice.appspot.com/_story_pics/img453cede493.jpeg" alt="" />
                         <p>I saw the Piggyvest advertisement on <br /> BBNaija show 2022 and checked the <br /> website. I was looking for so...</p>
                         <h2>— Christianah I</h2>
                     </div>
                     <div className='testimony-card' data-aos="fade-down" data-aos-delay="400">
-                        <img src="src/img/card4.jpeg" alt="" />
+                        <img src="https://storage.googleapis.com/piggybankservice.appspot.com/_story_pics/img5d2175e88f.jpeg" alt="" />
                         <p>Piggy vest has helped me learn how to save <br /> money and they also give an interest per <br /> annum</p>
                         <h2>— Ogbonna M</h2>
                     </div>

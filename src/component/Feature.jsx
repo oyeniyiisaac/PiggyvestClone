0dd2bf2bcb616035d46499469081f2edff6ca13a
oyeniyiisaac <isaacoyeniyi6@gmail.com>
2026-05-18 09:40:13 +0100

    fix: correct typo in image source URL in Feature component

--- src/component/Feature.jsx
+++ src/component/Feature.jsx
@@ -9,7 +9,7 @@ const feature = () => {
                 <div className='feature'>
                     <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/tclogo_2x_e3771e8b17/tclogo_2x_e3771e8b17.png" alt="" />
                     <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/output_9290be116f/output_9290be116f.png" alt="" />
-                    <img src="shttps://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/brand1_b2a9805fff/brand1_b2a9805fff.png" alt="" />
+                    <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/brand1_b2a9805fff/brand1_b2a9805fff.png" alt="" />
                     <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/pym_removebg_preview_109a666426/pym_removebg_preview_109a666426.svg" alt="" />
                     <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/fast_61fdc50524/fast_61fdc50524.svg" alt="" />
                     <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/cio_6806ee3afd/cio_6806ee3afd.svg" alt="" />

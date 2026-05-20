cf782a94b813e5e81ae60f5ae28544036e8f3934
oyeniyiisaac <isaacoyeniyi6@gmail.com>
2026-05-20 09:11:46 +0100

    style: uncomment Feature component in App and adjust layout styles in Feature.css for improved responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
             <Invest />
             <OpenHouseCard />
             <Testimony />
-            {/* <Feature /> */}
+            <Feature />
             {/* <Footer /> */}
         </>
     )
